1b84f2855f942eb0fb0a54a040d76fd01d815d19
tigawanna <denniskinuthiaw@gmail.com>
2024-07-14 18:32:23 +0300

    add theme switcher

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { useEffect } from "react";
+import { themeChange } from "theme-change";
 import { MainNavBar } from "./components/navbar/MainNavBar";
 
 function App() {
+  useEffect(() => {
+    themeChange(false);
+    // 👆 false parameter is required for react project
+  }, []);
   return (
-    <div className="min-h-screen w-full flex flex-col items-center ">
-      <MainNavBar/>
-
+    <div className="min-h-screen w-full flex flex-col items-center bg-base-100">
+      <MainNavBar />
     </div>
   );
 }
