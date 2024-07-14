5fd2e2329bd8ec3da14457519bc96980c2678a7c
tigawanna <denniskinuthiaw@gmail.com>
2024-07-14 17:17:21 +0300

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen w-full flex flex-col justify-center items-center">
+      <h1 className="text-3xl font-bold underline"> TWARK UI</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/park/ui/alert.tsx
@@ -0,0 +1,31 @@
+import { type HTMLArkProps, ark } from '@ark-ui/react/factory'
+import { type VariantProps, tv } from 'tailwind-variants'
+import { createStyleContext } from '~/components/park/lib/create-style-context'
+
+const alert = tv(
+  {
+    base: 'alert',
+    slots: {
+      root: 'alert__root',
+      content: 'alert__content',
+      description: 'alert__description',
+      icon: 'alert__icon',
+      title: 'alert__title',
+    },
+    variants: {},
+  },
+  { twMerge: false },
+)
+const { withProvider, withContext } = createStyleContext(alert)
+
+export interface RootProps extends HTMLArkProps<'div'>, VariantProps<typeof alert> {}
+
+export const Root = withProvider<HTMLDivElement, RootProps>(ark.div, 'root')
+
+export const Content = withContext<HTMLDivElement, HTMLArkProps<'div'>>(ark.div, 'content')
+
+export const Description = withContext<HTMLDivElement, HTMLArkProps<'div'>>(ark.div, 'description')
+
+export const Icon = withContext<HTMLOrSVGElement, HTMLArkProps<'svg'>>(ark.svg, 'icon')
+
+export const Title = withContext<HTMLHeadingElement, HTMLArkProps<'h5'>>(ark.h5, 'title')
